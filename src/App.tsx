c959ab999081c327070f0cdbf0771454f8e76c87
Simon Kurz <32772268+Ratsch0k@users.noreply.github.com>
2020-04-07 00:52:10 +0200

    Revert "Removed login, improved pipeline, added eslint" (#2)
    This reverts commit b8f40d4b5e6f36a0c2c90787d18ffa77c1693691.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import './App.css';
-import {createMuiTheme} from '@material-ui/core/styles';
-import {ThemeProvider} from '@material-ui/styles';
-import HeaderBar from './HeaderBar';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import HeaderBar from "./HeaderBar";
+
 
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#00C68A',
-      light: '#98F0D6',
-      dark: '#00A473',
-      contrastText: '#FFFFFF',
-    },
-    secondary: {
-      main: '#AD00D8',
-      light: '#ECA1FF',
-      dark: '#8900AB',
-      contrastText: '#FFFFFF',
-    },
-  },
+    palette: {
+        primary: {
+            main: "#00C68A",
+            light: "#98F0D6",
+            dark: "#00A473",
+            contrastText: "#FFFFFF"
+        },
+        secondary: {
+            main: "#AD00D8",
+            light: "#ECA1FF",
+            dark: "#8900AB",
+            contrastText: "#FFFFFF"
+        }
+    }
 });
 
 
 const App: React.FC = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <HeaderBar />
-      </div>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+           <div className="App">
+             <HeaderBar />
+           </div>
+        </ThemeProvider>
+    );
 };
 
 export default App;
--- src/HeaderBar.tsx
+++ src/HeaderBar.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import Avatar from './Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
-import MenuIcon from '@material-ui/icons/Menu';
-import AppBar from '@material-ui/core/AppBar/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import React from "react";
+import {makeStyles} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "./Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import EmojiTransportationIcon from "@material-ui/icons/EmojiTransportation";
+import MenuIcon from "@material-ui/icons/Menu";
+import AppBar from "@material-ui/core/AppBar/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
 
 const useStyles = makeStyles({
@@ -23,24 +23,18 @@ const useStyles = makeStyles({
 });
 
 const HeaderBar: React.FC = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <AppBar>
-      <Toolbar className={classes.root}>
-        <Typography className={classes.title} variant="h4">
-          Group Car
-        </Typography>
-        <Avatar iconName={classes.toolbarIcon} />
-        <IconButton color="inherit">
-          <EmojiTransportationIcon className={classes.toolbarIcon}/>
-        </IconButton>
-        <IconButton color="inherit">
-          <MenuIcon className={classes.toolbarIcon} />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
-  );
+    return(
+        <AppBar>
+            <Toolbar className={classes.root}>
+                <Typography className={classes.title} variant="h4">Group Car</Typography>
+                <Avatar iconName={classes.toolbarIcon} />
+                <IconButton color="inherit"><EmojiTransportationIcon className={classes.toolbarIcon}/></IconButton>
+                <IconButton color="inherit"><MenuIcon className={classes.toolbarIcon} /></IconButton>
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 export default HeaderBar;
