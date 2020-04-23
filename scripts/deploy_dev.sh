aabd606767fdd06169d26cc5efe3cfb965e684c7
Simon Kurz <simonlukas.98@gmail.com>
2020-04-23 20:59:04 +0200

    Added new deploy scripts, updated .travis.yml with new stages

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev
+set -e
 
 # encrypt key
 openssl aes-256-cbc -K $encrypted_e9ce4c9f2f9f_key -iv $encrypted_e9ce4c9f2f9f_iv -in deploy-key.enc -out deploy-key -d
@@ -10,8 +10,5 @@ mv deploy-key ~/.ssh/id_rsa
 # change to build directory, initialize local repository and push to server
 cd build
 
-# Delete files on server
-ssh $SERVER_USER@$SERVER_IP rm -R $SERVER_HTML_PATH/*
-
-# Copy build to server
-scp -r $PWD/* $SERVER_USER@$SERVER_IP:$SERVER_HTML_PATH
+# Replace existing html directory with updated one
+rsync --recursive --times --compress --delete --quiet ./ $SERVER_USER@$SERVER_IP:$SERVER_HTML_PATH_BETA
