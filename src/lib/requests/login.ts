eeb7bb83bd015ee0bbd6941d2514236602e92722
Simon Kurz <simonlukas.98@gmail.com>
2020-05-04 23:24:19 +0200

    Extracted login and signup requests

---
+++ src/lib/requests/login.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+type Request = import('./request').Request;
+
+/**
+ * Sends a login request to the backend with the given
+ * arguments.\
+ * If any arguments is either undefined/null or an empty string
+ * the request will not be send and will return a rejected promise.
+ * @param username  Username to sign up with
+ * @param password  The password for the account
+ */
+const login = (username: string, password: string): Request => {
+  // Check if provided arguments are non empty strings
+  if (!username || username.length <= 0 ||
+    !password || password.length <= 0) {
+    return {
+      request: Promise.reject(new Error('Request invalid')),
+      cancel: () => {},
+    };
+  }
+
+
+  const source = axios.CancelToken.source();
+
+  const request = axios.put(
+      '/auth/login',
+      {
+        username,
+        password,
+      },
+      {
+        cancelToken: source.token,
+      },
+  );
+
+  return {
+    request,
+    cancel: source.cancel,
+  };
+};
+
+export default login;
+
