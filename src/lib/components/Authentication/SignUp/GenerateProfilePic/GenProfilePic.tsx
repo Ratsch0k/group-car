4538d6b5c246802ef3e8d5a2ed98e52f0ee11921
Simon Kurz <simonlukas.98@gmail.com>
2020-05-05 14:59:47 +0200

    Added component for generating new profile pictures

---
+++ src/lib/components/Authentication/SignUp/GenerateProfilePic/GenProfilePic.tsx
@@ -0,0 +1,139 @@
+import React, {useEffect, useState} from 'react';
+import {makeStyles} from '@material-ui/styles';
+import {Grid, Button, Typography, Paper} from '@material-ui/core';
+import getRandomProfilePic from 'lib/requests/getRandomProfilePic';
+import {useTranslation} from 'react-i18next';
+import {grey} from '@material-ui/core/colors';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+type AxiosResponse = import('axios').AxiosResponse;
+type AxiosError = import('axios').AxiosError;
+type TCancel = import('axios').Cancel;
+
+
+interface GenerateProfilePicProps {
+  username: string;
+  setOffset(value: React.SetStateAction<number>): void;
+  offset: number;
+}
+
+const imgDim = 100;
+const iconDim = 120;
+
+const useStyle = makeStyles({
+  img: {
+    width: imgDim,
+    height: imgDim,
+  },
+  imgContainer: {
+    width: imgDim,
+    height: imgDim,
+    border: `2px solid ${grey[100]}`,
+    borderRadius: '50%',
+    overflow: 'hidden',
+  },
+  imgText: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: imgDim,
+    width: imgDim,
+    fontSize: '0.8rem',
+  },
+  imgIconAlt: {
+    fontSize: iconDim,
+  },
+});
+
+const GenerateProfilePic: React.FC<GenerateProfilePicProps> =
+(props: GenerateProfilePicProps) => {
+  const {username, offset, setOffset} = props;
+  const [data, setData] = useState<string>();
+  const {t} = useTranslation();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const incrementOffset = () => {
+    setOffset((prev: number) => prev + 1);
+  };
+
+  /**
+   * Every time either the username of the offset changes
+   * get the appropriate profile picture
+   */
+  useEffect(() => {
+    if (username.length > 0) {
+      const request = getRandomProfilePic(username, offset);
+
+      setLoading(true);
+      request.request.then((response: AxiosResponse) => {
+        setData(response.data);
+        setLoading(false);
+      }).catch((err: AxiosError | TCancel) => {
+        if (!(err.constructor.name === 'Cancel')) {
+          setData(undefined);
+          setLoading(false);
+        }
+      });
+
+      return () => request.cancel();
+    }
+  }, [username, offset]);
+
+  /**
+   * If the username changes reset the offset.
+   */
+  useEffect(() => {
+    setOffset(0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [username]);
+
+  const classes = useStyle();
+
+  return (
+    <Grid
+      container
+      direction='column'
+      justify='space-around'
+      alignItems='center'
+    >
+      <Grid
+        item
+
+      >
+        <Paper className={classes.imgContainer} elevation={5}>
+          {data ?
+          <img
+            className={classes.img}
+            src={data && URL.createObjectURL(data)}
+            alt={t('form.profilePicture.alt')}
+          /> :
+          <Typography align='center' className={classes.imgText}>
+            {
+              loading ?
+              t('misc.loading') :
+              <AccountCircleIcon className={classes.imgIconAlt} />
+            }
+          </Typography>
+
+          }
+        </Paper>
+      </Grid>
+      <Grid item>
+        <Button
+          onClick={incrementOffset}
+          color='secondary'
+          disabled={!username || username.length <= 0 || loading}
+        >
+          {
+            !username || username.length <= 0 ?
+            t('form.profilePicture.noUsername') :
+            t('form.profilePicture.generateNew')
+          }
+        </Button>
+      </Grid>
+    </Grid>
+
+  );
+};
+
+export default GenerateProfilePic;
--- src/lib/components/Avatar/Avatar.tsx
+++ src/lib/components/Avatar/Avatar.tsx
@@ -1,22 +1,46 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {makeStyles} from '@material-ui/styles';
+import {useTranslation} from 'react-i18next';
 
 interface AvatarProps {
-  username?: string;
+  userId?: number;
 }
 
+/**
+ * Dimension of avatar in header bar.
+ */
+const dim = 40;
+
+const useStyle = makeStyles({
+  img: {
+    borderRadius: '50%',
+    width: dim,
+    height: dim,
+  },
+  icon: {
+    fontSize: dim,
+  },
+});
+
 const Avatar: React.FC<AvatarProps> = (props: AvatarProps) => {
-  const {username} = props;
+  const {userId} = props;
+  const classes = useStyle();
+  const {t} = useTranslation();
 
-  useEffect(() => {
-    if (username) {
-
-    }
-  }, [username]);
-
-  return (
-    <AccountCircleIcon fontSize='large'/>
-  );
+  if (userId) {
+    return (
+      <img
+        className={classes.img}
+        src={`/api/user/${userId}/profile-pic`}
+        alt={t('misc.avatar')}
+      />
+    );
+  } else {
+    return (
+      <AccountCircleIcon className={classes.icon} />
+    );
+  }
 };
 
 export default Avatar;
