b3437d3affec946ae008415baa0159edf2fa3abe
Simon Kurz <simonlukas.98@gmail.com>
2020-05-07 23:04:50 +0200

    Added test for avatar component

--- src/lib/components/Avatar/Avatar.spec.tsx
+++ src/lib/components/Avatar/Avatar.spec.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from '@testing-library/react';
 import Avatar from './Avatar';
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Avatar iconName="name"/>, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders without userId', () => {
+  const {container} = render(<Avatar />);
+  expect(container).toMatchSnapshot();
+});
+
+it('renders with given userId', () => {
+  const userId = 12;
+  const {container} = render(<Avatar userId={userId} />);
+
+  expect(container).toMatchSnapshot();
+  expect(container.getElementsByTagName('img')[0])
+      .toHaveAttribute('src', `/api/user/${userId}/profile-pic`);
 });
