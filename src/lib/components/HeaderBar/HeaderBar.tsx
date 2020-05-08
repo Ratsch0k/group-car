58eb0e725f2134a986f0c5bb3dd23f0275ac9f9a
Simon Kurz <simonlukas.98@gmail.com>
2020-05-08 17:23:44 +0200

    Added UserOverview to show when useravatar clicked in headerbar if user logged in

--- src/lib/components/HeaderBar/HeaderBar.tsx
+++ src/lib/components/HeaderBar/HeaderBar.tsx
@@ -1,13 +1,14 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {makeStyles} from '@material-ui/core';
+import {makeStyles, Popover} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-import Avatar from '../Avatar';
+import UserAvatar from '../UserAvatar';
 import IconButton from '@material-ui/core/IconButton';
 import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
 import MenuIcon from '@material-ui/icons/Menu';
 import AppBar from '@material-ui/core/AppBar/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import AuthContext from 'lib/context/auth/authContext';
+import UserOverview from '../UserOverview/UserOverview';
 
 const useStyles = makeStyles({
   root: {
@@ -23,13 +24,19 @@ const HeaderBar: React.FC = () => {
   const classes = useStyles();
   const auth = useContext(AuthContext);
   const [userId, setUserId] = useState<number>();
+  const [userOverviewAnchor, setUserOverviewAnchor] =
+      useState<HTMLElement | null>(null);
 
-  const handleAvatarClick = () => {
+  const handleAvatarClick = (event: React.MouseEvent<HTMLElement>) => {
     if (auth.user && auth.user.username) {
-
+      setUserOverviewAnchor(event.currentTarget);
     } else {
       auth.openAuthDialog();
     }
+  };
+
+  const handleUserOverviewClose = () => {
+    setUserOverviewAnchor(null);
   };
 
   useEffect(() => {
@@ -47,10 +54,31 @@ const HeaderBar: React.FC = () => {
           Group Car
         </Typography>
         <IconButton color='inherit' onClick={handleAvatarClick}>
-          <Avatar
+          <UserAvatar
             userId={userId}
           />
         </IconButton>
+        <Popover
+          open={Boolean(userOverviewAnchor)}
+          onClose={handleUserOverviewClose}
+          anchorEl={userOverviewAnchor}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            horizontal: 'center',
+            vertical: 'top',
+          }}
+          transitionDuration={{
+            enter: 200,
+            exit: 200,
+          }}
+        >
+          <UserOverview
+            onClose={handleUserOverviewClose}
+          />
+        </Popover>
         <IconButton color='inherit'>
           <EmojiTransportationIcon fontSize='large'/>
         </IconButton>
