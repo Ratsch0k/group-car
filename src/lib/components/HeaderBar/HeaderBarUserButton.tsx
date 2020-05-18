36538688fdc5419683b07c347c4cbf73c3b1f63e
Simon Kurz <simonlukas.98@gmail.com>
2020-05-18 19:52:45 +0200

    Moved UserButton in header bar to own component

---
+++ src/lib/components/HeaderBar/HeaderBarUserButton.tsx
@@ -0,0 +1,91 @@
+import React, {useContext, useState, useEffect} from 'react';
+import {
+  ClickAwayListener,
+  Box,
+  IconButton,
+  Popper,
+  Fade,
+  Paper,
+} from '@material-ui/core';
+import UserAvatar from '../UserAvatar';
+import UserOverview from '../UserOverview/UserOverview';
+import AuthContext from 'lib/context/auth/authContext';
+
+const HeaderBarUserButton: React.FC = () => {
+  const auth = useContext(AuthContext);
+
+  const [userId, setUserId] = useState<number>();
+  const [anchor, setAnchor] =
+      useState<HTMLElement | null>(null);
+
+  /**
+   * Handles a click on the button.
+   * If the user is logged in the click will open
+   * the UserOverview Popper, if not the
+   * authentication dialog is opened.
+   * @param event MouseEvent to handle
+   */
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    if (auth.user && auth.user.username) {
+      if (anchor) {
+        setAnchor(null);
+      } else {
+        setAnchor(event.currentTarget);
+      }
+    } else {
+      auth.openAuthDialog();
+    }
+  };
+
+  /**
+   * Handles the close of the popper.
+   */
+  const handleClose = () => {
+    setAnchor(null);
+  };
+
+  // Update the user id if it changes.
+  useEffect(() => {
+    if (auth.user && auth.user.id !== undefined) {
+      setUserId(auth.user.id);
+    } else {
+      setUserId(undefined);
+    }
+  }, [auth.user]);
+
+  return (
+    <ClickAwayListener
+      onClickAway={handleClose}
+    >
+      <Box>
+        <IconButton color='inherit' onClick={handleClick}>
+          <UserAvatar
+            userId={userId}
+          />
+        </IconButton>
+
+        <Popper
+          open={Boolean(anchor)}
+          anchorEl={anchor}
+          placement='bottom'
+          disablePortal={true}
+          transition
+        >
+          {({TransitionProps}) => (
+            <Fade {...TransitionProps} timeout={200}>
+              <Paper
+                elevation={6}
+              >
+                <UserOverview
+                  onClose={handleClose}
+                />
+              </Paper>
+            </Fade>
+          )}
+        </Popper>
+      </Box>
+    </ClickAwayListener>
+  );
+};
+
+export default HeaderBarUserButton;
