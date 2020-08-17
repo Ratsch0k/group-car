24d1a39349fada8732df91b1c37e8691d8e22960
Simon Kurz <simon.kurz@rocketmail.com>
2020-08-17 12:55:20 +0200

    Fixed test issues

--- src/lib/components/Authentication/SignUp/SignUpForm.spec.tsx
+++ src/lib/components/Authentication/SignUp/SignUpForm.spec.tsx
@@ -4,6 +4,6 @@ import SignUpForm from './SignUpForm';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<SignUpForm />, div);
+  ReactDOM.render(<SignUpForm signUp={undefined as any}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
