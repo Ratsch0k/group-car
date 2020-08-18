b2e037aecb2ac97036c00cb7488535272e4b970b
Simon Kurz <simon.kurz@rocketmail.com>
2020-08-18 17:45:16 +0200

    Increased bottom padding in create group dialog

--- src/modals/group/CreateGroup/CreateGroup.tsx
+++ src/modals/group/CreateGroup/CreateGroup.tsx
@@ -3,21 +3,35 @@ import {
   AutoFullscreenDialog,
   CloseableDialogTitle,
   useModalRouter,
+  GroupCarTheme,
 } from 'lib';
 import {useTranslation} from 'react-i18next';
 import {DialogContent} from '@material-ui/core';
 import CreateGroupForm from './CreateGroupForm';
+import {makeStyles, createStyles} from '@material-ui/styles';
+
+/**
+ * Styles.
+ */
+const useStyles = makeStyles((theme: GroupCarTheme) =>
+  createStyles({
+    content: {
+      paddingBottom: theme.spacing(3),
+    },
+  }),
+);
 
 export const CreateGroup: React.FC = () => {
   const {close} = useModalRouter();
   const {t} = useTranslation();
+  const classes = useStyles();
 
   return (
     <AutoFullscreenDialog open={true} fullWidth>
       <CloseableDialogTitle close={close}>
         {t('modals.group.create.title')}
       </CloseableDialogTitle>
-      <DialogContent>
+      <DialogContent className={classes.content}>
         <CreateGroupForm />
       </DialogContent>
     </AutoFullscreenDialog>
