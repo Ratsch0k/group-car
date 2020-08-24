96b2a5000b353256f85b3c8f93761c2b1c06e2b1
Simon Kurz <simon.kurz@rocketmail.com>
2020-08-24 16:54:14 +0200

    Started on tests

---
+++ src/modals/group/ManageGroup/ManageGroup.spec.tsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import ManageGroup from "./ManageGroup";
+import React from "react";
+
+it('renders without crashing', () => {
+  render(<ManageGroup />);
+});
