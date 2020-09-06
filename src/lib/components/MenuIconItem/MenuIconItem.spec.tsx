f80bd286b62d2acc00de4a1929041a969e13c89e
Simon Kurz <simon.kurz@rocketmail.com>
2020-09-06 22:59:01 +0200

    Added tests for new components

---
+++ src/lib/components/MenuIconItem/MenuIconItem.spec.tsx
@@ -0,0 +1,64 @@
+import { render } from '@testing-library/react';
+import MenuIconItem from "./MenuIconItem";
+import React from "react";
+import { ThemeProvider, MenuItem } from '@material-ui/core';
+import { theme } from 'lib/theme';
+import AddIcon from '@material-ui/icons/Add'
+
+it('renders without crashing', () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <MenuIconItem>
+      TEST
+      </MenuIconItem>
+    </ThemeProvider>
+  );
+});
+
+it('matches MenuItem if no property provided', () => {
+  const {container} = render(
+    <ThemeProvider theme={theme}>
+      <MenuIconItem>
+        TEST
+      </MenuIconItem>
+    </ThemeProvider>
+  );
+  
+  expect(container).toMatchSnapshot();
+});
+
+it('displays icon on the left if icon is provided', () => {
+  const {container} = render(
+    <ThemeProvider theme={theme}>
+      <MenuIconItem icon={<AddIcon />}>
+        TEST
+      </MenuIconItem>
+    </ThemeProvider>
+  );
+  
+  expect(container).toMatchSnapshot();
+});
+
+it('shows arrow to the right if prop opensSubMenu is true', () => {
+  const {container} = render(
+    <ThemeProvider theme={theme}>
+      <MenuIconItem opensSubMenu>
+        TEST
+      </MenuIconItem>
+    </ThemeProvider>
+  );
+  
+  expect(container).toMatchSnapshot();
+});
+
+it('shows arrow to the right if prop opensSubMenu is true and icon on the left if icon is provided', () => {
+  const {container} = render(
+    <ThemeProvider theme={theme}>
+      <MenuIconItem opensSubMenu icon={<AddIcon />}>
+        TEST
+      </MenuIconItem>
+    </ThemeProvider>
+  );
+  
+  expect(container).toMatchSnapshot();
+});
