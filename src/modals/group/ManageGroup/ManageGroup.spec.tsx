5698cd4897c7a3750ac9994b71b98074bab4f961
Simon Kurz <simon.kurz@rocketmail.com>
2020-09-14 23:22:26 +0200

    Added new snapshot test for checking if member list is rendered correctly

--- src/modals/group/ManageGroup/ManageGroup.spec.tsx
+++ src/modals/group/ManageGroup/ManageGroup.spec.tsx
@@ -1,31 +1,35 @@
 import { render, waitFor } from "@testing-library/react";
 import ManageGroup from "./ManageGroup";
 import React from "react";
-import { Api, theme, GroupContext, AuthContext, GroupWithOwnerAndMembers, IUser} from '../../../lib';
+import { theme, GroupContext, AuthContext, GroupWithOwnerAndMembers, IUser} from '../../../lib';
 import { MemoryRouter, Route } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 
-const fakeGroup: GroupWithOwnerAndMembers = {
-  id: 2,
-  Owner: {
+let fakeGroup: GroupWithOwnerAndMembers;
+
+beforeEach(() => {
+  fakeGroup = {
     id: 2,
-    username: "TEST",
-  },
-  ownerId: 2,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  name: 'fake group name',
-  description: 'fake group description',
-  members: [
-    {
-      User: {
-        id: 2,
-        username: 'TEST',
-      },
-      isAdmin: true,
-    }
-  ]
-};
+    Owner: {
+      id: 2,
+      username: "TEST",
+    },
+    ownerId: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    name: 'fake group name',
+    description: 'fake group description',
+    members: [
+      {
+        User: {
+          id: 2,
+          username: 'TEST',
+        },
+        isAdmin: true,
+      }
+    ]
+  };
+}); 
 
 it('renders without crashing', () => {
   render(
@@ -51,7 +55,7 @@ it('renders circular progress while loading group data',() => {
   );
 
   expect(screen.baseElement).toMatchSnapshot();
-})
+});
 
 it('renders error message if group doesn\'t exist', async () => {
   const fakeApi = {
@@ -73,10 +77,9 @@ it('renders error message if group doesn\'t exist', async () => {
   expect(screen.queryByText('modals.group.manage.loadingFailed')).not.toBeNull();
 
   expect(screen.baseElement).toMatchSnapshot();  
-})
+});
 
-it('renders group name and description if group exists', async () => {
-
+it('renders group info if group exists', async () => {
   const fakeApi = {
     getGroup: jest.fn().mockResolvedValue({data: fakeGroup}),
   }
@@ -102,7 +105,33 @@ it('renders group name and description if group exists', async () => {
   // Check if name and description exist
   expect(screen.queryByText(fakeGroup.name)).not.toBeNull();
   expect(screen.queryByText(fakeGroup.description)).not.toBeNull();
-})
+});
+
+it('renders list of members correctly', async () => {
+  // Add more members to fake group data
+  fakeGroup.members.push({User: {id: 12, username: 'ADMIN'}, isAdmin: true});
+  fakeGroup.members.push({User: {id: 13, username: 'MEMBER'}, isAdmin: false});
+  const fakeApi = {
+    getGroup: jest.fn().mockResolvedValue({data: fakeGroup}),
+  };
+
+  const screen = render (
+    <ThemeProvider theme={theme}>
+      <MemoryRouter>
+        <AuthContext.Provider value={{user: fakeGroup.Owner as IUser} as AuthContext}>
+          <GroupContext.Provider value={fakeApi as unknown as GroupContext}>
+              <ManageGroup groupId={2}/>
+          </GroupContext.Provider>
+        </AuthContext.Provider>
+      </MemoryRouter>
+    </ThemeProvider>
+  );
+
+  await waitFor(() => expect(fakeApi.getGroup).toHaveBeenCalledTimes(1));
+  expect(fakeApi.getGroup).toHaveBeenLastCalledWith(fakeGroup.id);
+
+  expect(screen).toMatchSnapshot();
+});
 
 it('get groupId from route params if not provided as property', async () => {
   const fakeApi = {
@@ -125,4 +154,4 @@ it('get groupId from route params if not provided as property', async () => {
   expect(fakeApi.getGroup).toHaveBeenLastCalledWith(fakeGroup.id);
 
   expect(screen.baseElement).toMatchSnapshot();
-})
+});
