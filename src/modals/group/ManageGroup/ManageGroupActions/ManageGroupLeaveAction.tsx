477b304cb6bfe04006c4323babdf57d896253918
Simon Kurz <simon.kurz@rocketmail.com>
2020-10-21 23:35:03 +0200

    Added a confirm dialog when leaving a group

--- src/modals/group/ManageGroup/ManageGroupActions/ManageGroupLeaveAction.tsx
+++ src/modals/group/ManageGroup/ManageGroupActions/ManageGroupLeaveAction.tsx
@@ -1,3 +1,11 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
 import {red} from '@material-ui/core/colors';
 import {makeStyles} from '@material-ui/styles';
 import {ProgressButton, useGroups, useModalRouter, useSnackBar} from 'lib';
@@ -34,29 +42,60 @@ export const ManageGroupLeaveAction: React.FC<ManageGroupLeaveActionProps> =
   const {groupId} = props;
   const {close} = useModalRouter();
   const {show} = useSnackBar();
+  const [open, setOpen] = useState<boolean>(false);
 
-  const handleClick = async () => {
+  const handleLeave = async () => {
     setLoading(true);
     try {
       await leaveGroup(groupId);
       setLoading(false);
-      show('success', t('modals.group.manage.leaveGroupSuccess'));
+      show('success', t('modals.group.manage.leaveGroup.success'));
       close();
     } catch {
       setLoading(false);
     }
   };
 
+  const handleClick = () => {
+    setOpen(true);
+  };
+
   return (
-    <ProgressButton
-      variant='outlined'
-      className={classes.button}
-      fullWidth
-      loading={loading}
-      onClick={handleClick}
-    >
-      {t('modals.group.manage.leaveGroup')}
-    </ProgressButton>
+    <>
+      <Button
+        variant='outlined'
+        className={classes.button}
+        fullWidth
+        onClick={handleClick}
+      >
+        {t('modals.group.manage.leaveGroup.button')}
+      </Button>
+      <Dialog open={open} onBackdropClick={() => setOpen(false)}>
+        <DialogTitle>
+          {t('modals.group.manage.leaveGroup.dialog.title')}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {t('modals.group.manage.leaveGroup.dialog.content')}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => setOpen(false)}
+            disabled={loading}
+          >
+            {t('misc.no')}
+          </Button>
+          <ProgressButton
+            onClick={handleLeave}
+            loading={loading}
+            color='primary'
+          >
+            {t('misc.yes')}
+          </ProgressButton>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
