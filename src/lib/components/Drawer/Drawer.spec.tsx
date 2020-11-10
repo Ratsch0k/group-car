847fc202603e7d589cfa392b91d234222110737d
Simon Kurz <simon.kurz@rocketmail.com>
2020-11-10 16:57:04 +0100

    Integrated list of cars into drawer

--- src/lib/components/Drawer/Drawer.spec.tsx
+++ src/lib/components/Drawer/Drawer.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 import {ThemeProvider} from '@material-ui/core';
-import {theme, Drawer} from 'lib';
+import {theme, Drawer, AuthContext, GroupContext} from 'lib';
 
 it('renders and matches snapshot with open and ' +
     'not permanent without crashing', () => {
@@ -45,4 +45,44 @@ it('renders and matches snapshot with not open and ' +
   );
 
   expect(baseElement).toMatchSnapshot();
+});
+
+
+it('renders NotLoggedIn component if user is not logged in', () => {
+  const authContext = {
+    isLoggedIn: false,
+  };
+
+  const {baseElement} = render(
+    <ThemeProvider theme={theme}>
+      <AuthContext.Provider value={authContext}>
+        <Drawer open={false} onClose={jest.fn} permanent={false}/>
+      </AuthContext.Provider>
+    </ThemeProvider>,
+  );
+
+  expect(baseElement).toMatchSnapshot();
+});
+
+describe('if user is logged in', () => {
+  const customRender = (
+    authContext: AuthContext,
+    groupContext: GroupContext,
+  ) => {
+    return render(
+        <ThemeProvider theme={theme}>
+          <AuthContext.Provider value={authContext}>
+            <GroupContext.Provider value={groupContext}>
+              <Drawer open={false} onClose={jest.fn} permanent={true}/>
+            </GroupContext.Provider>
+          </AuthContext.Provider>
+        </ThemeProvider>
+    );
+  };
+
+  it('renders create group button if user has no groups', () => {
+    const {baseElement} = customRender({isLoggedIn: true}, {groups: []});
+
+    expect(baseElement).toMatchSnapshot();
+  });
 });
