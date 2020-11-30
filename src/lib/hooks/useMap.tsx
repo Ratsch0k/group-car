204905e493eca4e905e8dd8bf9408b659624389d
Simon Kurz <simon.kurz@rocketmail.com>
2020-11-30 21:27:39 +0100

    Added missing comment

--- src/lib/hooks/useMap.tsx
+++ src/lib/hooks/useMap.tsx
@@ -1,6 +1,9 @@
 import {MapContext} from 'lib';
 import {useContext} from 'react';
 
+/**
+ * Shortcut hook for accessing the MapContext.
+ */
 export const useMap = (): MapContext => {
   return useContext(MapContext);
 };
