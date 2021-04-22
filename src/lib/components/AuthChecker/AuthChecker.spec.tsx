763c9d87601e25b0a6879c977a40f28cd11d9a93
Simon Kurz <simon.kurz@rocketmail.com>
2021-04-23 00:25:24 +0200

    Added test for AuthChecker

--- src/lib/components/AuthChecker/AuthChecker.spec.tsx
+++ src/lib/components/AuthChecker/AuthChecker.spec.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {render, waitFor} from '@testing-library/react';
-import {ModalContext} from '../../ModalRouter/ModalRouteContext';
-import { MemoryRouter } from 'react-router-dom';
+import {waitFor} from '@testing-library/react';
 import testRender from '../../../__test__/testRender';
+import mockedAxios from '../../../__test__/mockAxios';
 import AuthChecker from '../AuthChecker/AuthChecker';
 
 
@@ -36,12 +35,149 @@ describe('AuthChecker', () => {
     describe('reacts to AxiosErrors', () => {
 
       describe('if response is NotLoggedInError', () => {
-        it('and user is logged in, reset auth state');
+        it(
+          'and user is logged in,  dispatches reset auth state action',
+          async () => {
+          const state = {
+            auth: {
+              user: {
+                username: 'TEST USER',
+                id: 1,
+              },
+            },
+          };
 
-        it('and user is not logged in');
+          // Get error handler
+          let errorHandler;
+          mockedAxios.interceptors.response.use = jest.fn().mockImplementation((_, fn) => {
+            errorHandler = fn;
+          });
+
+          const {store} = testRender(
+            state,
+            <AuthChecker />,
+          );
+    
+          const fakeError = {
+            response: {
+              data: {
+                detail: {
+                  errorName: 'NotLoggedInError',
+                },
+              }
+            }
+          }
+
+          let errorThrown = false;
+          try {
+            await errorHandler(fakeError);
+          } catch {
+            errorThrown = true;
+          }
+          expect(errorThrown).toBeTruthy();
+
+          const expectedAction = {
+            type: 'auth/reset',
+            payload: undefined,
+          };
+          
+          const actions = store.getActions();
+          expect(actions).toContainEqual(expectedAction);
+          expect(actions).toHaveLength(3);
+        });
+
+        it('and user is not logged in, do nothing', async() => {
+          const state = {
+            auth: {
+              user: undefined,
+            },
+          };
+
+          // Get error handler
+          let errorHandler;
+          mockedAxios.interceptors.response.use = jest.fn().mockImplementation((_, fn) => {
+            errorHandler = fn;
+          });
+
+          const {store} = testRender(
+            state,
+            <AuthChecker />,
+          );
+    
+          const fakeError = {
+            response: {
+              data: {
+                detail: {
+                  errorName: 'NotLoggedInError',
+                },
+              }
+            }
+          }
+
+          let errorThrown = false;
+          try {
+            await errorHandler(fakeError);
+          } catch {
+            errorThrown = true;
+          }
+          expect(errorThrown).toBeTruthy();
+
+          const notExpectedAction = {
+            type: 'auth/reset',
+            payload: undefined,
+          };
+          
+          const actions = store.getActions();
+          expect(actions).not.toContainEqual(notExpectedAction);
+          expect(actions).toHaveLength(2);
+        });
       });
 
-      it('if not NotLoggedInError, do nothing');
+      it('if not NotLoggedInError, do nothing', async () => {
+        const state = {
+          auth: {
+            user: undefined,
+          },
+        };
+
+        // Get error handler
+        let errorHandler;
+        mockedAxios.interceptors.response.use = jest.fn().mockImplementation((_, fn) => {
+          errorHandler = fn;
+        });
+
+        const {store} = testRender(
+          state,
+          <AuthChecker />,
+        );
+  
+        const fakeError = {
+          response: {
+            data: {
+              detail: {
+                errorName: 'OtherError',
+              },
+            }
+          }
+        }
+
+        let errorThrown = false;
+        try {
+          await errorHandler(fakeError);
+        } catch {
+          errorThrown = true;
+        }
+        expect(errorThrown).toBeTruthy();
+
+        const notExpectedAction = {
+          type: 'auth/reset',
+          payload: undefined,
+        };
+        
+        const actions = store.getActions();
+        expect(actions).not.toContainEqual(notExpectedAction);
+        expect(actions).toHaveLength(2);
+      });
     })
 
 });
