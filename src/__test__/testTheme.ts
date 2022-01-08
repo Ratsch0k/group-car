04b93e7590f483390ffd393d79aa7c6e0551e51f
Simon Kurz <simonlukas.98@gmail.com>
2022-01-08 16:27:53 +0100

    Added width control to testRender and testTheme

--- src/__test__/testTheme.ts
+++ src/__test__/testTheme.ts
@@ -1,12 +1,16 @@
 import {createTheme} from '@material-ui/core';
+// IMPORTANT: Do not shorten import.
+// Otherwise, index magic will break the axios mock during tests
 import {themeProperties, IThemeOptions} from '../lib/theme';
 
-export default createTheme({
+
+export default (width?: string) => createTheme({
   ...themeProperties,
   props: {
     MuiButtonBase: {
       disableTouchRipple: true,
       disableRipple: true,
     },
+    MuiWithWidth: {initialWidth: width},
   },
 } as IThemeOptions);
