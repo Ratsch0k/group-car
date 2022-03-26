0df2062beaa03da2dc442251bf2b7b999d08e13c
Simon Kurz <simonlukas.98@gmail.com>
2022-03-26 22:45:17 +0100

    Moved api types from index.d.ts to index.ts due to hinting issues

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,85 @@
+import {GroupWithOwner} from './group';
+import {UserSimple} from '../../typings';
+
+/**
+ * The invite of the specified user for the specified group.
+ */
+export interface Invite {
+  /**
+   * The id of the user who is invited.
+   */
+  userId: number;
+  /**
+   * The id of the group for which this invite is.
+   */
+  groupId: number;
+  /**
+   * The date when this invite was created.
+   */
+  createdAt: Date;
+  /**
+   * The date when this invite was last updated.
+   */
+  updatedAt: Date;
+  /**
+   * The id of the user who created this invite.
+   */
+  invitedBy: number;
+}
+
+/**
+ * Extension of the Invite which includes the data of the invited user.
+ */
+export interface InviteUser {
+  /**
+   * The data of the invited user.
+   */
+  User: UserSimple;
+}
+
+/**
+ * Group data for invites.
+ */
+export interface InviteGroup {
+  /**
+   * Group of the invite.
+   */
+  Group: GroupWithOwner;
+}
+
+/**
+ * Data of the sender of the invite.
+ */
+export interface InviteSender {
+  /**
+   * Invite sender.
+   */
+  InviteSender: UserSimple;
+}
+
+/**
+ * Invites which include the user and the invite sender.
+ */
+export type InviteWithUserAndInviteSender = Invite & InviteUser & InviteSender;
+
+/**
+ * Invite which includes the group and the invite sender data.
+ */
+export type InviteWithGroupAndInviteSender = Invite &
+InviteGroup &
+InviteSender;
+
+/**
+ * Versions of backend systems.
+ */
+export interface BackendVersions {
+  backend: string;
+}
+
+
 export * from './getRandomProfilePic';
 export * from './group';
 export * from './request.d';
 export * from './auth';
 export * from './user';
 export * from './getVersions';
-export * from './index.d';
