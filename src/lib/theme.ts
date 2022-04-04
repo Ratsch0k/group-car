ff205677e18ee8dac74ffb68b9b306bae74536ca
Simon Kurz <simonlukas.98@gmail.com>
2022-04-04 21:22:13 +0200

    Changed theme

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,33 +1,44 @@
-import {createTheme} from '@material-ui/core';
+import {createTheme, Theme, ThemeOptions} from '@material-ui/core';
 import {green, orange} from '@material-ui/core/colors';
-
-type Theme = import('@material-ui/core').Theme;
-type ThemeOptions = import('@material-ui/core').ThemeOptions;
-type Shape = import('@material-ui/core/styles/shape').Shape;
+import {Palette} from '@material-ui/core/styles/createPalette';
+import {Shape} from '@material-ui/core/styles/shape';
 
 export interface HeaderHeight {
   small: number;
   default: number;
 }
 
+export interface Sized<T> {
+  default: T;
+  small: T;
+  large: T;
+}
+
 interface IShape extends Shape {
   headerHeight: HeaderHeight;
   drawerWidth: number;
+  borderRadiusSized: Sized<number>
+}
+
+interface IPalette extends Palette {
+  blur: string;
 }
 
 export interface IThemeOptions extends ThemeOptions {
   shape: Partial<IShape>;
+  palette: Partial<IPalette>;
 }
 
 export interface GroupCarTheme extends Theme {
   shape: IShape;
+  palette: IPalette;
 }
 
 export const themeProperties: IThemeOptions = {
   palette: {
     primary: {
       main: '#00C68A',
-      light: '#bfecdb',
+      light: '#8edcc0',
       dark: '#00A473',
       contrastText: '#FFFFFF',
     },
@@ -49,6 +60,19 @@ export const themeProperties: IThemeOptions = {
       dark: orange['800'],
       contrastText: '#FFFFFF',
     },
+    background: {
+      default: '#f5f5f5',
+      paper: '#ffffff',
+    },
+    blur: 'blur(8px)',
+  },
+  mixins: {
+    toolbar: {
+      'minHeight': 46,
+      '@media(min-width: 600px)': {
+        minHeight: 64,
+      },
+    },
   },
   shape: {
     headerHeight: {
@@ -56,6 +80,12 @@ export const themeProperties: IThemeOptions = {
       default: 64,
     },
     drawerWidth: 350,
+    borderRadius: 10,
+    borderRadiusSized: {
+      default: 10,
+      large: 16,
+      small: 8,
+    },
   },
 };
 
