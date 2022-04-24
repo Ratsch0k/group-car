fbc4772d111562fb67e984445e049423d0ca6107
Simon Kurz <simonlukas.98@gmail.com>
2022-04-24 11:20:58 +0200

    v1.0.0-beta.0

--- src/config/index.ts
+++ src/config/index.ts
@@ -41,7 +41,7 @@ const config: Config = {
     dsn: process.env.SENTRY_DSN || 'https://46304bd186a44341a70545d48b23647b@o656739.ingest.sentry.io/5762871',
     tracesSampleRate: 1.0,
   },
-  frontend: '1.0.0-alpha.1',
+  frontend: '1.0.0-beta.0',
 };
 
 export default config;
