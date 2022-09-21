3e0e246974177bd0d46c5aaa253b5c850a563fc1
Simon Kurz <simonlukas.98@gmail.com>
2022-09-21 15:56:35 +0200

    Added docs to Alert component

--- src/lib/components/Alert.tsx
+++ src/lib/components/Alert.tsx
@@ -1,6 +1,9 @@
 import {withStyles} from '@material-ui/core';
 import AlertMui from '@material-ui/lab/Alert';
 
+/**
+ * Styled Alert.
+ */
 export const Alert = withStyles({
   root: {
     boxShadow: '-4px 4px 16px 0px rgba(171, 171, 171, 0.25),' +
